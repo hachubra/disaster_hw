0b46092f0a54c7aedc952c6d4a399f2e7fb048e9
alex <alex@ubu04.localdomain>
2024-08-03 19:57:06 +0500

    fix_zadanie2.1

--- 2/commands.sh
+++ 2/commands.sh
@@ -7,3 +7,14 @@ python3 -m http.server 9999 --bind 0.0.0.0
 sudo apt update
 sudo apt-get install haproxy
 
+sudo nano /etc/haproxy/haproxy.cfg 
+
+sudo systemctl reload haproxy
+
+curl https://127.0.0.1:8088
+
+cd ~/disaster/disaster/2/http2
+python3 -m http.server 9999 --bind 0.0.0.0
+
+
+curl -H 'Host:example.local' http://127.0.0.1:8088
